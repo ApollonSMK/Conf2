4f57bae27519d4894e39ad4c4a6568de81b11e63
Pablo Silva <microgramxx@gmail.com>
2025-09-05 20:45:20 +0000

    Try fixing this error: `Build Error: Export Grapes doesn't exist in targ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ const Destaques = [
   },
 ];
 
-export default function InicioPage() {
+export default function Page() {
   return (
     <div className="flex flex-col min-h-[calc(100vh-5rem)]">
       <section className="flex-grow flex items-center justify-center text-center py-20 px-4 sm:px-6 lg:px-8">
