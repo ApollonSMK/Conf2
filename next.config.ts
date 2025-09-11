a0412b1145555d21be9a1a6ce05848934c6a5234
Pablo Silva <microgramxx@gmail.com>
2025-09-11 17:29:08 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,10 +28,21 @@ const nextConfig: NextConfig = {
         hostname: 'bucket.xyziow3.space',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bucket.confrariasportugal.pt',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '89a01d3a07aec76b3caaaac5d9c71e51.r2.cloudflarestorage.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
 };
 
 export default nextConfig;
-
